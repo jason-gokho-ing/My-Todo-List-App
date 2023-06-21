7ab64ca40b816412ba1126185243ecc692506e99
jason gokho ing <jason.gokho.ing@gmail.com>
2023-06-20 17:46:16 -0700

    Initial Commit

--- functions.py
+++ functions.py
@@ -7,12 +7,6 @@ def read_todo_list(filepath=FILEPATH):
     return local_todo_list
 
 
-""" Read a text file and return the list of items """
-
-
 def write_todo_list(todo_list_arg, filepath=FILEPATH):
     with open(filepath, "w") as write_file:
         write_file.writelines(todo_list_arg)
-
-
-""" writes the todo_list from the 'todo_list.txt' file """
